de4f825f79819a09bce3784bdc8a00d6849f3d9d
Alexander P. Valencia <alex16jpv@gmail.com>
2021-07-06 01:08:03 -0500

    add basic info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Alex's blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Alexander Valencia`,
+      summary: ``,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `Simple blog.`,
+    siteUrl: `https://https://blog.alexpvalencia.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `alexpvalencia`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["AUTO", "WEBP", "AVIF"]}
-        src="../images/profile-pic.png"
+        src="../images/profile-pic.jpeg"
         width={50}
         height={50}
         quality={95}
@@ -45,10 +45,6 @@ const Bio = () => {
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
         </p>
       )}
     </div>
